badd4674faa4187f695ba5ab9c05eaec18800a4c
Artur Harkovchuk <artcrimechanel@gmail.com>
2018-05-05 12:27:32 +0300

    add test

--- gtest/Test_TheButcher/googletest_level.h
+++ gtest/Test_TheButcher/googletest_level.h
@@ -2,13 +2,126 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+#include <stdio.h>
+#include <iostream>
+#include "../../Level/level.h"
+using namespace std;
 
-using namespace testing;
 
-TEST(TheButcherCase1, TheButcherSet1)
+
+TEST(TheButcherCaseLevel, TheButcherSet_VerificationOfCreationLevel)
 {
-    EXPECT_EQ(1, 1);
-    ASSERT_THAT(0, Eq(0));
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+    if (a == NULL)
+         FAIL();
+    else
+         SUCCEED();
 }
+
+TEST(TheButcherCaseHero, TheButcherSet_CheckHeroLevelStart)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroLevel();
+  // cout << hl << endl;
+    if (hl != 0) // the first level of the hero is zero
+         FAIL();
+    else
+         SUCCEED();
+}
+
+TEST(TheButcherCaseHero, TheButcherSet_CheckHeroExpStart)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroExp();
+   // cout << hl << endl;
+    if (hl != 0) // the first Exp of the hero is zero
+         FAIL();
+    else
+         SUCCEED();
+}
+
+TEST(TheButcherCaseHero, TheButcherSet_CheckBaseDamage)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroBaseDamage();
+    cout << hl << endl;
+    if (hl != 0)
+         FAIL();
+    else
+         SUCCEED();
+}
+
+
+
+
+TEST(TheButcherCaseHeroGun, TheButcherSet_CheckHeroGunLevel)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroGunLevel();
+   // cout << hl << endl;
+    if (hl != 0)
+         FAIL();
+    else
+         SUCCEED();
+}
+
+TEST(TheButcherCaseHeroGun, TheButcherSet_CheckGunExp)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroGunExp();
+   // cout << hl << endl;
+    if (hl != 0)
+         FAIL();
+    else
+         SUCCEED();
+}
+
+TEST(TheButcherCaseHeroMage, TheButcherSet_CheckMageLevel)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroMageLevel();
+   // cout << hl << endl;
+    if (hl != 0)
+         FAIL();
+    else
+         SUCCEED();
+}
+
+TEST(TheButcherCaseHeroMage, TheButcherSet_CheckMageExp)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroMageExp();
+   // cout << hl << endl;
+    if (hl != 0)
+         FAIL();
+    else
+         SUCCEED();
+}
+
+
+
+TEST(TheButcherCaseHeroMelee, TheButcherSet_CheckMeleeLevel)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroMeleeLevel();
+   // cout << hl << endl;
+    if (hl != 0)
+         FAIL();
+    else
+         SUCCEED();
+}
+
+TEST(TheButcherCaseHeroMelee, TheButcherSet_CheckMeleeExp)
+{
+    Level *a = new Level{1,1,1,1,1,1,1,1,1,1};
+   int hl =a->getHeroMeleeExp();
+   // cout << hl << endl;
+    if (hl != 0)
+         FAIL();
+    else
+         SUCCEED();
+}
+
+
 
 
