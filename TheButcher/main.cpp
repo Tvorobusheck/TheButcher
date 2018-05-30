a76905a0efbe1434e9af4c37ba28299167e8878e
Artur Harkovchuk <artcrimechanel@gmail.com>
2018-05-30 13:58:07 +0300

    qml+cpp

--- TheButcher/main.cpp
+++ TheButcher/main.cpp
@@ -1,19 +1,23 @@
 #include "Level/level.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <qqml.h>
 #include <Qt>
 
 int main(int argc, char *argv[])
 {
 #if defined(Q_OS_WIN)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
-    QGuiApplication app(argc, argv);
+QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/TheButcher_logics.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
-    return app.exec();
+QQmlApplicationEngine engine;
+qmlRegisterType<Level>( "CustomComponents", 1, 0, "Level" );
+engine.load(QUrl(QStringLiteral("qrc:/TheButcher_logics.qml")));
+if (engine.rootObjects().isEmpty())
+return -1;
+
+return app.exec();
+
 }
